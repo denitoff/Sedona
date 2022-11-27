4cf83691f225a7b5473417db4a4f525678af85c2
denitoff <denitoff@gmail.com>
2022-11-27 19:42:32 +0530

    реализована сборка проекта в build(доработан gulpfile + package.json). сборка спрайта. минификация стилей. оптимизация изображений

--- source/js/script.js
+++ source/js/script.js
@@ -1,3 +1,42 @@
+// mobileMenu
+
+var siteNavigationDropdown = document.querySelector(".site-navigation__dropdown");
+var siteNavigationList = document.querySelector(".site-navigation__list");
+let windowInnerWidth = window.innerWidth;
+
+
+siteNavigationDropdown.classList.remove("site-navigation__dropdown--none");
+if (windowInnerWidth < 768) {
+  siteNavigationList.classList.add("site-navigation__list--close");
+
+}
+else {
+
+  siteNavigationDropdown.classList.remove("site-navigation__dropdown--burger");
+  siteNavigationDropdown.classList.add("site-navigation__dropdown--cross");
+}
+
+
+siteNavigationDropdown.addEventListener("click", function () {
+  if (siteNavigationDropdown.classList.contains("site-navigation__dropdown--burger")) {
+    siteNavigationList.classList.remove("site-navigation__list--close");
+    siteNavigationDropdown.classList.remove("site-navigation__dropdown--burger");
+    siteNavigationDropdown.classList.add("site-navigation__dropdown--cross");
+
+  }
+
+  else {
+    siteNavigationList.classList.add("site-navigation__list--close");
+    siteNavigationDropdown.classList.remove("site-navigation__dropdown--cross");
+    siteNavigationDropdown.classList.add("site-navigation__dropdown--burger");
+  }
+
+});
+
+
+
+// formPopup
+
 let popupErr = document.querySelector(".popup--error");
 let popupSuccess = document.querySelector(".popup--success");
 let buttonSubmit = document.querySelector(".review-form__button");
